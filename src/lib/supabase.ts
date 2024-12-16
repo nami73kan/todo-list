c0eb75706d8c10ebf70c249d9eaefdcc6c587930
MATSUMURA NAMI <nami-t-73@hotmail.co.jp>
2024-12-16 17:41:21 +0900

    API確認

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,10 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
-if (!supabaseUrl || !supabaseKey) {
-    throw new Error('Supabase URL and Key are required');
-  }
-  
-export const supabase = createClient(supabaseUrl, supabaseKey);
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Supabase URL and Key are required');
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
